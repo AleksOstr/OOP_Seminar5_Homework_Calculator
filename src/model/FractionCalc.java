02eaad2125c63b049963f89c9ac43b43fd13b54f
AleksOstr <ostryankoam@yandex.ru>
2023-01-16 20:29:02 +0400

    completed FractionCalc

--- src/model/FractionCalc.java
+++ src/model/FractionCalc.java
@@ -12,16 +12,28 @@ public class FractionCalc implements Calculable<Num.Fraction>{
 
     @Override
     public Num.Fraction diff(Num.Fraction num1, Num.Fraction num2) {
-        return null;
+        long numer1 = num1.getNumerator();
+        long numer2 = num2.getNumerator();
+        long denom1 = num1.getDenominator();
+        long denom2 = num2.getDenominator();
+        return new Num.Fraction(numer1 * denom2 - numer2 * denom1, denom1 * denom2).simplify();
     }
 
     @Override
     public Num.Fraction mult(Num.Fraction num1, Num.Fraction num2) {
-        return null;
+        long numer1 = num1.getNumerator();
+        long numer2 = num2.getNumerator();
+        long denom1 = num1.getDenominator();
+        long denom2 = num2.getDenominator();
+        return new Num.Fraction(numer1 * numer2, denom1 * denom2).simplify();
     }
 
     @Override
     public Num.Fraction div(Num.Fraction num1, Num.Fraction num2) {
-        return null;
+        long numer1 = num1.getNumerator();
+        long numer2 = num2.getNumerator();
+        long denom1 = num1.getDenominator();
+        long denom2 = num2.getDenominator();
+        return new Num.Fraction(numer1 * denom2, numer2 * denom1).simplify();
     }
 }
