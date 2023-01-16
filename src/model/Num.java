bfde2ea016297e7eb1f77c4a50919f545a8e0e51
AleksOstr <ostryankoam@yandex.ru>
2023-01-16 19:22:23 +0400

    add Num class and Fraction subclass

---
+++ src/model/Num.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Num {
+    public static class Fraction extends Num {
+        private long numerator;
+        private long denominator;
+
+        public Fraction(long numerator, long denominator) {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public long getNumerator() {
+            return numerator;
+        }
+
+        public long getDenominator() {
+            return denominator;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%d/%d", this.numerator, this.denominator);
+        }
+
+        public Fraction simplify() {
+            long limit = Math.min(Math.abs(numerator), denominator);
+            for (int i = 2; i <= limit ; i++) {
+                if (numerator % i == 0 && denominator % i == 0) {
+                    numerator /= i;
+                    denominator /= i;
+                }
+            }
+            return this;
+        }
+    }
+}
