16c392d12d3f8ff99ff483f616327c14184dfcbe
AleksOstr <ostryankoam@yandex.ru>
2023-01-16 19:24:04 +0400

    add Calculable interface

--- src/model/Calculable.java
+++ src/model/Calculable.java
@@ -1,4 +1,8 @@
 package model;
 
-public interface Calculable {
+public interface Calculable<T extends Num> {
+    T sum(T num1, T num2);
+    T diff(T num1, T num2);
+    T mult(T num1, T num2);
+    T div(T num1, T num2);
 }
