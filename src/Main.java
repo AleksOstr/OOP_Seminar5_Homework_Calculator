9b54a0b009585853c0d82c2dd3e5110d16965b58
AleksOstr <ostryankoam@yandex.ru>
2023-01-17 08:17:25 +0400

    add Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
+import controller.ComplexController;
+import controller.FractionController;
+import model.ComplexCalc;
+import model.FractionCalc;
+import view.CalcView;
+
 public class Main {
+    public static void main(String[] args) {
+        FractionCalc fractionCalc = new FractionCalc();
+        ComplexCalc complexCalc = new ComplexCalc();
+        FractionController fracController = new FractionController(fractionCalc);
+        ComplexController compController = new ComplexController(complexCalc);
+        CalcView view = new CalcView(compController, fracController);
+        view.run();
+    }
 }
